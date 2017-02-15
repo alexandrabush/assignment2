e6121d0782b30a556fb184f891b3c8f4ada257ae
Alex Bush <alexandrabush1@gmail.com>
2017-02-14 20:17:58 -0500

    Added a success page and images folder.

--- JS/form.js
+++ JS/form.js
@@ -1,20 +1,21 @@
 function validateForm() {
     var x = document.forms["pizzaForm"]["crust"].value;
     if (x == "") {
-        alert("Crust type must be selected");
+        alert("Crust type must be selected! Please pick a crust type to continue.");
         return false;
     }
 
     var x = document.forms["pizzaForm"]["sauce"].value;
     if (x == "") {
-        alert("Sauce type must be selected");
+        alert("Sauce type must be selected! Please pick a sauce type to continue.");
         return false;
     }
 
-    var x = document.forms["myForm"]["cheese"].value;
+    var x = document.forms["pizzaForm"]["cheese"].value;
     if (x == "") {
-        alert("Cheese type must be selected");
+        alert("Cheese type must be selected! Please pick a cheese type to continue.");
         return false;
     }
-}
 
+    return true;
+}
